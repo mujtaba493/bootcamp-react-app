2757d35059254b4a8884db0f3590fa2725a35326
Mujtaba Hussain <hmujtaba@gmail.com>
2020-06-10 06:38:16 +0500

    updated app.js again

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
   <div className="App">
-  <header className="App-header">
-   <img src={logo} className="App-logo" alt="logo"/>
-   <p>
-     Edit <code>src/app.js</code> and save to reload
-     </p>
-    <a
-     className="App-link" 
-     href="https://reactjs.org"
-     target="blank"
-     rel="noopener noreferrer"
-     >
-       learn react
-     </a>
-  </header>
+    <h1>Hello World From Mujtaba Hussain</h1>
+    <h2>This is Assignment No 2 </h2>
   </div>
 
   );
