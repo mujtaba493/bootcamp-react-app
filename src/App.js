e45b7fc537941553aa1b4afbf9c9a183a0b27956
Mujtaba Hussain <hmujtaba@gmail.com>
2020-06-11 12:50:20 +0500

    updated app.js on thursday 11 june 2020

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
   <div className="App">
-    <h1>Hello World From Mujtaba Hussain</h1>
+    <h1>Hello World Bootcamp From Mujtaba Hussain</h1>
     <h2>This is Assignment No 2 </h2>
   </div>
 
